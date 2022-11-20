5f56e1f1b8267dd780902e020bb13cb5a8f1be26
Jon Nichols <nichojo89@gmail.com>
2022-11-20 15:14:13 -0500

    Sum of left leaves algorithm

---
+++ app/src/main/java/com/nichols/dsa/tree/SumOfLeftLeaves.java
@@ -0,0 +1,26 @@
+package com.nichols.dsa.tree;
+
+public class SumOfLeftLeaves {
+    public int solveSumOfLeft(TreeNode A) {
+        int res = 0;
+
+        if(A != null){
+            if(isLeaf(A.left))
+                res += A.left.val;
+            else
+                res += solveSumOfLeft(A.left);
+
+            res += solveSumOfLeft(A.right);
+        }
+
+        return res;
+    }
+
+    public boolean isLeaf(TreeNode A){
+        if(A == null)
+            return false;
+        if(A.left == null && A.right == null)
+            return true;
+        return false;
+    }
+}
