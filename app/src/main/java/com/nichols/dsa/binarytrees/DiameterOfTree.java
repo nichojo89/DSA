5d178e91f833a14ef8ccc71000cd53796bc486cc
Jon Nichols <nichojo89@gmail.com>
2023-03-04 15:57:47 -0500

    Solve Diameter of tree algorithm

--- app/src/main/java/com/nichols/dsa/binarytrees/DiameterOfTree.java
+++ app/src/main/java/com/nichols/dsa/binarytrees/DiameterOfTree.java
@@ -10,6 +10,22 @@ public class DiameterOfTree {
             val = x;
         }
     }
+
+    public int solveDiameterOfTree(TreeNode A) {
+        int[] diameter = new int[1];
+        height(A, diameter);
+        return diameter[0];
+    }
+
+    private int height(TreeNode node, int[] diameter) {
+        if (node == null) {
+            return 0;
+        }
+        int lh = height(node.left, diameter);
+        int rh = height(node.right, diameter);
+        diameter[0] = Math.max(diameter[0], lh + rh);
+        return 1 + Math.max(lh, rh);
+    }
     public int isBalanced(TreeNode A)
     {
         int lh;
